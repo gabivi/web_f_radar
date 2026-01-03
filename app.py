6aeb76d7c014040bb9c91e6b1540b2f894e9417c
gabivi <gabivi@gmail.com>
2026-01-03 17:35:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ TEMPLATE = r"""
         }
 
         let icon = ''
-        if (p.name !== 'here' or p.name !== '.') {
+        if (p.name !== 'here' ) {
              icon = makePlaneDivIcon(rot, isSelected);
             } else {
              icon = makeStaticDivIcon(rot, isSelected);
